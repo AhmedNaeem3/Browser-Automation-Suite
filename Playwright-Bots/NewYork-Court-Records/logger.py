8b53764fe576c6cd8d401dff2edc937523335e53
Ahmed Naeem <ahmednaeem2345@gmail.com>
2026-02-12 19:38:35 +0500

    Updated logger of newyork court scraper

--- Playwright-Bots/NewYork-Court-Records/logger.py
+++ Playwright-Bots/NewYork-Court-Records/logger.py
@@ -4,14 +4,10 @@ import logging
 
 
 def setup_logging():
-    # Create a custom logger
     logger = logging.getLogger("MarylandScraper")
     logger.setLevel(logging.INFO)
-
-    # Create formatters and add them to the handlers
     formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 
-    # Create file handlers for info and error logs
     info_handler = logging.FileHandler("info.log", mode="a", encoding="utf-8")
     info_handler.setLevel(logging.INFO)
     info_handler.setFormatter(formatter)
@@ -20,7 +16,6 @@ def setup_logging():
     error_handler.setLevel(logging.ERROR)
     error_handler.setFormatter(formatter)
 
-    # Add the handlers to the logger
     if not logger.handlers:
         logger.addHandler(info_handler)
         logger.addHandler(error_handler)
